8b1f3150efe27ddc2dcd551d740297ea69072ada
Jorge Ernesto Chavez Puente <jrgchvzpnt@gmail.com>
2023-12-05 17:00:23 -0700

    first commit

---
+++ config/database_config.py
@@ -0,0 +1,9 @@
+# config/database_config.py
+
+DB_CONFIG = {
+    'server': 'tu_servidor_sql_server',
+    'database': 'tu_base_de_datos',
+    'username': 'tu_usuario',
+    'password': 'tu_contraseña',
+    'driver': 'ODBC Driver 17 for SQL Server',
+}
---
+++ config/pdf_config.py
@@ -0,0 +1,6 @@
+# config/pdf_config.py
+
+PDF_CONFIG = {
+    'output_path': '/ruta/donde/guardar/pdf',
+    'barcode_text': 'tu_texto_de_código_de_barras',
+}
